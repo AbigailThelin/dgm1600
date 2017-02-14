7ab4ac7762e0f106a640476648a479dfc4984470
Abigail Thelin <abbyjthelin@gmail.com>
2017-02-14 11:17:53 -0700

    Else Clause
    The examples and discription of what an else clause are in the code

--- ifstatements.cs
+++ ifstatements.cs
@@ -3,66 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ifstatements : MonoBehaviour {
-	private int A = 10;
-	private int B = 3;
-	private string name = "Joseph";
-	private bool study = true;
-	private float Gandalf = 6.5f;
-	private float Sam = 4.4f;
-
-	/*
-	 * ==
-	 * =
-	 * <
-	 * 
-	 * .
-	 * <=
-	 * >=
-	 */
-
+	private int worktime = 8;
 
 	// Use this for initialization
 	void Start () {
-		if (Sam < Gandalf)
-			print ("Same is never greater than Gandalf.");
-
-		{
-			if (Gandalf == Sam)
-				print ("Gandalf is always greater.");
+		if (worktime <= 9){
+			print("Go to work right NOW.");
 		}
-		if(name != "Amber")
-	
-			print("My name is Joseph.");
-
-		if (A == B) 
-		{
-			print ("A is equal to B");
+		else if(worktime == 9){
+			print ("Welcome to work.");
 		}
-		if (A != B) 
-		{
-			print ("A is not equal to B");
+		if (worktime <= 10){
+			print("get ready for meeting with Jim.");
 		}
-		if (A > B) 
-		{
-			print ("A is more than B");
+		else if(worktime == 10){
+			print ("Meeting with Jim.");
 		}
-		if (A < B) 
-		{
-			print ("A is less than B");
+		if (worktime <= 12){
+			print("Make Lunch reservations for McDonalds.");
 		}
-		if (A <= B) 
-		{
-			print ("A is less than or equal to B");
+		else if(worktime == 12){
+			print ("Head to Lunch.");
 		}
-		if (A >= B) 
-		{
-			print ("A is greater than or equal to B");
+		if (worktime <= 1){
+			print("Start finishing up lunch.");
 		}
-		if (study != true) 
-		{
-			print ("GO STUDY YOU FOOL.");
+		else if(worktime == 12){
+			print ("Head back to the workplace.");
 		}
-		
+		if (worktime <= 15){
+			print("Finish up your work so you can head home.");
+		}
+		else if(worktime == 15){
+			print ("HEAD HOME");
 		}
 
+		}
 }
+ 
+//* if statements are true then it will go to the if statments print line, but if not it will go to the else if. There can be multiple else if statements in a block of code but there can only be one else statement at the end of a block of code. 
